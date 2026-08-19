6e1fe59f8faf2554056593b458de26f307fdb2af
Katia2019 <katia_lazzara@icloud.com>
2026-08-18 21:48:10 -0300

    Add files via upload

---
+++ Pixel Store/script.js
@@ -0,0 +1,9 @@
+function filtrar()
+{
+    let pc1 = document.getElementById("computador1");
+    let pc2 = document.getElementById("computador2");
+
+
+    pc1.classList.add("esconder");
+    pc2.classList.add("esconder");
+}
